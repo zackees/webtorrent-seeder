50b17ad40f3511fcb19d4f7123ec441fda8f66d5
zackees <zachvorhies@protonmail.com>
2022-07-14 16:28:51 -0400

    adds warning for unsupported webrtc client

--- index.js
+++ index.js
@@ -2,23 +2,55 @@
 // https://clients.dh2i.com/NatTest/
 
 const WebTorrent = require('webtorrent')
-var prompt = require('prompt');
-const client = new WebTorrent()
+const prompt = require('prompt')
 
-function doSeed(trackers, files) {
-    console.log("Seeding: ", files, " with trackers", trackers)
-    client.seed(files, { announce: trackers }, torrent => {
-        console.log(`Seeded: ${torrent.name}\nmagnetURI: ${torrent.magnetURI}`)
-    })
-    client.on("error", err => {
-        console.error(err)
-    });
+const DEFAULT_TRACKER = 'wss://webtorrent-tracker.onrender.com:80'
+
+const WEBTORRENT_CONFIG = {
+  tracker: {
+    rtcConfig: {
+      iceServers: [
+        {
+          urls: [
+            'stun:stun.l.google.com:19302',
+            'stun:stun1.l.google.com:19302',
+            'stun:stun2.l.google.com:19302',
+            'stun:global.stun.twilio.com:3478'
+          ]
+        }
+      ],
+      sdpSemantics: 'unified-plan',
+      bundlePolicy: 'max-bundle',
+      iceCandidatePoolsize: 1
+    }
+  }
+}
+
+if (!WebTorrent.WEBRTC_SUPPORT) {
+  console.error('This browser is unsupported. Please use a browser with WebRTC support.')
+}
+
+const webtorrentClient = new WebTorrent(WEBTORRENT_CONFIG)
+
+function doSeed (trackers, files) {
+  console.log('Seeding: ', files, ' with trackers', trackers)
+  webtorrentClient.seed(files, { announce: trackers }, torrent => {
+    console.log(`Seeded: ${torrent.name}\nmagnetURI: ${torrent.magnetURI}`)
+  })
+  webtorrentClient.on('error', err => {
+    console.error(err)
+  })
 }
 
 prompt.start()
 prompt.get(['tracker', 'file'], function (err, result) {
+  if (err) {
+    console.error(err)
+  } else {
     result.file = result.file || './test.mp4'
-    result.tracker = result.tracker || 'wss://tracker.openwebtorrent.com'
+    result.tracker = result.tracker || DEFAULT_TRACKER
     const FILES = [result.file]
     doSeed([result.tracker], FILES)
-});
+  }
+})
+
